bbc5fe0fa213f8cb7a3bafcffee310267ba84372
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-04-30 16:49:59 -0300

    removi prefixo da rota pra melhorar usabilidade

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.api.transacao import router as transacao_router
 app = FastAPI()
 
 # Registrar as rotas
-app.include_router(transacao_router, prefix="/api/v1/transacoes", tags=["Transações"])
+app.include_router(transacao_router, tags=["Transações"])
 
 
 # Rota para indicar que a API iniciou
