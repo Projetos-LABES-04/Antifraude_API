c8393dad97130b09f1429c306e5f34825f641bfb
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-05-02 14:38:23 -0300

    adicionando validação do id

--- app/models/models.py
+++ app/models/models.py
@@ -1,8 +1,21 @@
 from pydantic import BaseModel, Field, validator
 from typing import Optional
 from datetime import datetime
+from bson import ObjectId
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("ID inválido do MongoDB")
+        return str(v)  # Retorna como string para compatibilidade com JSON
 
 class Transacao(BaseModel):
+    _id: PyObjectId = Field(..., alias="_id")
     transacao_id: str = Field(..., min_length=1)
     cliente_id: int
     conta_id: str
@@ -18,3 +31,9 @@ class Transacao(BaseModel):
         if v.lower() not in tipos_validos:
             raise ValueError(f"Tipo de transação inválido: {v}")
         return v.lower()
+
+    class Config:
+        allow_population_by_field_name = True
+        json_encoders = {
+            ObjectId: str
+        }
