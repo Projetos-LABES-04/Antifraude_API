de74ca797716003f14efe9dbb048569a5e614844
nalandareis <131488457+nalandareis@users.noreply.github.com>
2025-05-02 17:34:59 -0300

    senha +segura

--- app/routes/usuario.py
+++ app/routes/usuario.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+import re
 from fastapi import HTTPException, status
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
@@ -18,6 +19,31 @@ router = APIRouter(
 class LoginModel(BaseModel):
     nome: str
     senha: str
+
+@field_validator('senha')
+def validar_senha(cls, v):
+        # Verifica se há pelo menos um caractere especial
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', v):
+            raise ValueError("A senha deve conter pelo menos um caractere especial")
+        # Verifica se há pelo menos uma letra maiúscula
+        if not re.search(r'[A-Z]', v):
+            raise ValueError("A senha deve conter pelo menos uma letra maiúscula")
+        # Verifica se há pelo menos uma letra minúscula
+        if not re.search(r'[a-z]', v):
+            raise ValueError("A senha deve conter pelo menos uma letra minúscula")
+        # Verifica se há pelo menos um número
+        if not re.search(r'[0-9]', v):
+            raise ValueError("A senha deve conter pelo menos um número")
+        # Verifica o comprimento mínimo
+        if len(v) < 8:
+            raise ValueError("A senha deve ter pelo menos 8 caracteres")
+        return v
+
+
+
+
+
+
 
 @router.post('/login')
 def login_usuario(data: LoginModel):
