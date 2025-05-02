3be0a15afc6a55b2bf548f8a3ccc37278d8be7cf
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-05-02 14:53:48 -0300

    testando rota

--- app/schemas/transacao_schema.py
+++ app/schemas/transacao_schema.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from typing import Literal
 
 class Transacao(BaseModel):
-    _id: str = Field(..., example="680fe24ca2db5716d502f3ec")
+    id: str = Field(..., alias="_id", example="680fe24ca2db5716d502f3ec")  # Usando alias
     transacao_id: str = Field(..., example="8c6a2055")
     cliente_id: int = Field(..., gt=0, example=356)
     conta_id: str = Field(..., example="1cbe0f3f1")
@@ -12,3 +12,7 @@ class Transacao(BaseModel):
     transacao_data: datetime = Field(..., example="2023-01-01T00:00:30")
     transacao_valor: float = Field(..., gt=0, example=2474.5)
     transacao_tipo: Literal["pix", "boleto", "cartao", "transferencia"] = Field(..., example="pix")
+
+    class Config:
+        # Configurar o Pydantic para aceitar alias
+        allow_population_by_field_name = True
