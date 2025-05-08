cb9f951b36289f22f7b7e06261c2d49e8ffd47f9
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-05-08 16:53:43 -0300

    adicionando um valor fixo pra simular como seria a checagem real

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,3 +2,22 @@ from fastapi import APIRouter, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from app.utils.auth_utils import criar_token
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+class LoginRequest(BaseModel):
+    email: str
+    senha: str
+
+@router.post("/login")
+async def login(data: LoginRequest):
+    # 💡 Aqui você colocaria a VERDADEIRA checagem no banco
+    if data.email == "usuario@empresa.com" and data.senha == "123456":
+        # Gera o token JWT contendo o email como identificação
+        token = criar_token({"sub": data.email})
+        return {
+            "access_token": token,
+            "token_type": "bearer"
+        }
+    else:
+        raise HTTPException(status_code=401, detail="Credenciais inválidas")
