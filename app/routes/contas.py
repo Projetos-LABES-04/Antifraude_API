192ea0ea0eb32f5f70cefd767e8212d54db0af51
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-05-20 01:28:46 -0300

    refazendo rota

--- app/routes/contas.py
+++ app/routes/contas.py
@@ -7,33 +7,4 @@ router = APIRouter()
 
 @router.get("/contas/analise",response_model=list[ContaResumo])
 async def listar_resumo_contas():
-    try:
-        data_limite = datetime.utcnow() - timedelta(days=90)
-
-        pipeline = [
-            { "$match": { "transacao_data": { "$gte": data_limite } } },
-            { "$sort": { "transacao_data": -1 } },
-            {
-                "$group": {
-                    "_id": "$conta_id",
-                    "cliente_id": { "$first": "$cliente_id" },
-                    "ultima_atividade": { "$first": "$transacao_data" },
-                    "valor_ultima": { "$first": "$transacao_valor" }
-                }
-            },
-            {
-                "$project": {
-                    "conta_id": "$_id",
-                    "cliente_id": 1,
-                    "ultima_atividade": 1,
-                    "valor_ultima": 1,
-                    "status": { "$literal": "análise" }
-                }
-            }
-        ]
-
-        resultado = await db["todo_collection"].aggregate(pipeline).to_list(length=None)
-        return resultado
     
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
