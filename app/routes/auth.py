882e4d419d337f1c77058a8fe37b49e36a79dd77
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-05-20 16:21:58 -0300

    criando rota login

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,3 +2,15 @@ from fastapi import APIRouter, HTTPException
 from app.db.database import db
 from app.schemas.auth_schema import LoginInput
 from app.utils.auth_utils import verificar_senha, gerar_token
+
+router = APIRouter()
+
+@router.post("/login")
+async def login(dados: LoginInput):
+    usuario = await db["usuarios"].find_one({"matricula": dados.matricula})
+    
+    if not usuario or not verificar_senha(dados.senha, usuario["hashed_senha"]):
+        raise HTTPException(status_code=401, detail="Matrícula ou senha inválidos.")
+    
+    token = gerar_token({"sub": usuario["matricula"], "nome": usuario["nome"]})
+    return {"access_token": token, "token_type": "bearer"}
