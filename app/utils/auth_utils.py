506271ec57c1ea8c79b0ea51a65c3c44f1dcec4c
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-05-20 20:11:59 -0300

    atualizei

--- app/utils/auth_utils.py
+++ app/utils/auth_utils.py
@@ -7,14 +7,13 @@ SECRET_KEY = "chave_muito_segura"
 ALGORITHM = "HS256"
 EXPIRE_MINUTES = 60
 
-#Esse objeto vai cuidar de gerar e verificar hashes de senhas com segurança.
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-#gerar_hash_senha
+
 def gerar_hash_senha(senha: str) -> str:
     return pwd_context.hash(senha)
 
-#Compara a senha digitada (senha_plana) com o hash armazenado (senha_hash)
 def verificar_senha(senha_plana: str, senha_hash: str) -> bool:
     return pwd_context.verify(senha_plana, senha_hash)
 
