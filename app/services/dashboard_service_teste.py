5957f1ba99f8617204c5e390cadae942589c4711
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-05-29 17:51:33 -0300

    adicionando service

---
+++ app/services/dashboard_service_teste.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+from app.db.database import db
+
+async def contar_transacoes_periodo(periodo_inicio: datetime, periodo_fim: datetime) -> dict:
+    transacoes_col = db["transacoes"]
+
+    dias = []
+    data_atual = periodo_inicio
+    while data_atual <= periodo_fim:
+        dias.append(data_atual.strftime("%Y-%m-%d"))
+        data_atual += timedelta(days=1)
+
+    filtro_data = {
+        "transacao_data": {
+            "$regex": f"^({'|'.join(dias)})"
+        }
+    }
+
+    total = await transacoes_col.count_documents(filtro_data)
+    return {"quantidade_transacoes": total}
