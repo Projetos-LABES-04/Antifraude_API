b3cc020c0b3391cb24a5e64766c791f3dbf0a95a
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-06-09 22:04:40 -0300

    Adicionei condição para caso não haja filtro retornar total de transações no sistema

--- app/services/dashboard_service_teste.py
+++ app/services/dashboard_service_teste.py
@@ -1,19 +1,22 @@
 from datetime import datetime, timedelta
 from app.db.database import db
+from typing import Optional
 
-async def contar_transacoes_periodo(periodo_inicio: datetime, periodo_fim: datetime) -> dict:
+async def contar_transacoes_periodo(periodo_inicio: Optional[datetime], periodo_fim: Optional[datetime]) -> dict:
     transacoes_col = db["todo_collection"]
 
-    # formato YYYY-MM-DD
-    dias = []
-    data_atual = periodo_inicio
-    while data_atual <= periodo_fim:
-        dias.append(data_atual.strftime("%Y-%m-%d "))
-        data_atual += timedelta(days=1)
+    if periodo_inicio and periodo_fim:
+        dias = []
+        data_atual = periodo_inicio
+        while data_atual <= periodo_fim:
+            dias.append(data_atual.strftime("%Y-%m-%d "))
+            data_atual += timedelta(days=1)
 
-    filtro_data = {
-        "$or": [{"transacao_data": {"$regex": f"^{dia}"}} for dia in dias]
-    }
+        filtro_data = {
+            "$or": [{"transacao_data": {"$regex": f"^{dia}"}} for dia in dias]
+        }
+    else:
+        filtro_data = {}
 
     print("📅 Dias utilizados no filtro:", dias)
     print("🔎 Filtro montado:", filtro_data)
