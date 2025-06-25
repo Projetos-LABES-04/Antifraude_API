0404f151e159114fcbe0c0ef04f61e373330c6fb
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-06-25 14:42:53 -0300

    adicionei payload

--- app/services/ml_client.py
+++ app/services/ml_client.py
@@ -1,21 +1,19 @@
  # Cliente responsável por se comunicar com o serviço ML
 import httpx
 
-ML_API_URL = "https://web-production-0e5e.up.railway.app/predict" 
+ML_API_URL = "https://web-production-995a.up.railway.app/inferencia"
 
 async def chamar_servico_ml(transacao: dict) -> int:
     
     payload = {
-        "transacao_valor": transacao["transacao_valor"],
-        "fim_de_semana": transacao["fim_de_semana"],
-        "transacao_tipo_pix": transacao["transacao_tipo_pix"],
-        "transacao_tipo_transferencia": transacao["transacao_tipo_transferencia"],
-        "erro_reconstrucao": transacao["erro_reconstrucao"],
-        "distancia_cluster": transacao["distancia_cluster"],
-        "mesma_titularidade": transacao["mesma_titularidade"],
-        "faixa_horaria_Madrugada": transacao["faixa_horaria_Madrugada"],
-        "dia_de_semana_Sabado": transacao["dia_de_semana_Sabado"],
-        "dia_de_semana_Domingo": transacao["dia_de_semana_Domingo"]
+        "transacao_id": "abc123",
+        "cliente_id": 101,
+        "conta_id": "c1",
+        "conta_destino_id": "c2",
+        "mesma_titularidade": False,
+        "transacao_data": "2023-08-01T08:15:00",
+        "transacao_valor": 1250.0,
+        "transacao_tipo": "pix",
     }
 
     async with httpx.AsyncClient() as client:
