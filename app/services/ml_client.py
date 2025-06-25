faf2e6798e5450eed62adec74ef20712843c4bf3
AllanVic0 <allan.maia@sergipetec.org.br>
2025-06-25 16:42:45 -0300

    Atualização ml_client

--- app/services/ml_client.py
+++ app/services/ml_client.py
@@ -5,23 +5,28 @@ from datetime import datetime
 ML_API_URL = "https://web-production-995a.up.railway.app/inferencia"
 
 async def chamar_servico_ml(transacao: dict) -> dict:
-    # Garante que a data esteja no formato ISO
-    if isinstance(transacao["transacao_data"], datetime):
-        transacao["transacao_data"] = transacao["transacao_data"].isoformat()
+    try:
+        if isinstance(transacao["transacao_data"], datetime):
+            transacao["transacao_data"] = transacao["transacao_data"].isoformat()
 
-    # Monta o payload com apenas os campos esperados
-    payload = {
-        "transacao_id": transacao["transacao_id"],
-        "cliente_id": transacao["cliente_id"],
-        "conta_id": transacao["conta_id"],
-        "conta_destino_id": transacao["conta_destino_id"],
-        "mesma_titularidade": transacao["mesma_titularidade"],
-        "transacao_data": transacao["transacao_data"],
-        "transacao_valor": transacao["transacao_valor"],
-        "transacao_tipo": transacao["transacao_tipo"]
-    }
+        payload = {
+            "transacao_id": str(transacao["transacao_id"]),
+            "cliente_id": int(transacao["cliente_id"]),
+            "conta_id": str(transacao["conta_id"]),
+            "conta_destino_id": str(transacao["conta_destino_id"]),
+            "mesma_titularidade": bool(transacao["mesma_titularidade"]),
+            "transacao_data": str(transacao["transacao_data"]),
+            "transacao_valor": float(transacao["transacao_valor"]),
+            "transacao_tipo": str(transacao["transacao_tipo"])
+        }
 
-    async with httpx.AsyncClient() as client:
-        response = await client.post(ML_API_URL, json=[payload])  # Lista com uma transação
-        response.raise_for_status()
-        return response.json()["amostra"][0]  # Retorna o primeiro item da resposta
+        async with httpx.AsyncClient() as client:
+            response = await client.post(ML_API_URL, json=[payload])
+            response.raise_for_status()
+            return response.json()["amostra"][0]
+
+    except httpx.HTTPStatusError as e:
+        print("Erro ao enviar para /inferencia:")
+        print(payload)
+        print("Erro:", e.response.text)
+        raise
